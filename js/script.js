0c46ed4a4c8e872e718127c07352e7b46d77891b
jimmyw32234 <jim.w32234@gmail.com>
2020-01-23 04:29:44 +0000

    xo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 let player = "X"
 let turnNumber = 0
-let booleanVar= true;
+let booleanVar= false;
 
 function performLogic(buttonId, tileId) {
     $(buttonId).remove()
     $(tileId).html(player)
     changePlayer();
     changeTurn();
+    checkVerticalWins();
+    checkHorizontalWins();
+    checkDiagonalWins();
 }
 
 function changePlayer(){
@@ -19,13 +22,35 @@ function changePlayer(){
 
 function changeTurn() {
     turnNumber = turnNumber + 1;
-    if (turnNumber===9) {
+    if (turnNumber === 9) {
     $("h1").html("It's a draw!");
     }
 }
 
+function checkWin(tileId1,tileId2,tileId3) {
+    if ($(tileId1).html() === player && $(tileId2).html() === player && $(tileId3).html() === player) 
+        {
+        $("h1").text(player + " wins!");
+        booleanVar = true;
+    }
+}
 
+function checkVerticalWins() {
+    checkWin("#tile1", "#tile4", "#tile7"); 
+    checkWin("#tile2", "#tile5", "#tile8"); 
+    checkWin("#tile3", "#tile6", "#tile9");
+}
 
+function checkDiagonalWins() {
+    checkWin("#tile1", "#tile5", "#tile9"); 
+    checkWin("#tile3", "#tile5", "#tile7"); 
+}
+
+function checkHorizontalWins() {
+    checkWin("#tile1", "#tile2", "#tile3"); 
+    checkWin("#tile4", "#tile5", "#tile6"); 
+    checkWin("#tile7", "#tile8", "#tile9");
+}
 
 
 $("#button1").click(function() {
